f4143337b674b8fe57f66ac63d8543417a23f435
黄智健 <782389483@qq.com>
2018-11-26 11:22:20 +0800

    1

--- AliLogin.php
+++ AliLogin.php
@@ -4,7 +4,7 @@ namespace aliLogin;
 
 abstract  class  AliLogin{
 
-    //回调地址,appid
+    //回调地址,appid ，test
     abstract  function alilogin($url, $app_id);
     //appid，私钥，阿里公钥，auth_code
     abstract  function aliRedirect($appId,$rsaPrivateKey,$alipayrsaPublicKey,$auth_code);
